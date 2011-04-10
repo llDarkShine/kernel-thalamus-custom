55ed75e3ca35ec077143a0d609b760ee6a4b7215
Michal Potrzebicz <m.potrzebicz@gmail.com>
2011-04-11 01:06:37 +0200

    New CPUfreq governor based on ondemand and conservative. Initial commit

---
+++ drivers/cpufreq/cpufreq_hybrid.c
@@ -0,0 +1,9 @@
+/*
+ *  drivers/cpufreq/cpufreq_hybrid.c
+ *
+ *  Copyright (C)  2011 Michal Potrzebicz
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+ */

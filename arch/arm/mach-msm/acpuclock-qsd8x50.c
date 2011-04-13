54794200aba8cdd302e970dbc0f0dfc8867e6f77
Michal Potrzebicz <m.potrzebicz@gmail.com>
2011-04-13 13:59:11 +0200

    Added more CPU frequency steps to keep it idling as low as possible. Undervolted CPU at lowest frequencies a little.

--- arch/arm/mach-msm/acpuclock-qsd8x50.c
+++ arch/arm/mach-msm/acpuclock-qsd8x50.c
@@ -74,9 +74,12 @@ struct clkctl_acpu_speed {
 struct clkctl_acpu_speed acpu_freq_tbl[] = {
         {  19200, CCTL(CLK_TCXO, 1),            SRC_RAW, 0, 0, 975, 14000 },
         { 128000, CCTL(CLK_TCXO, 1),            SRC_AXI, 0, 0, 975, 14000 },
-        { 245000, CCTL(CLK_MODEM_PLL, 1),       SRC_RAW, 0, 0, 1000, 29000 },
+        { 245000, CCTL(CLK_MODEM_PLL, 1),       SRC_RAW, 0, 0, 975, 29000 },
         /* Work arround for acpu resume hung, GPLL is turn off by arm9 */
         /*{ 256000, CCTL(CLK_GLOBAL_PLL, 3),      SRC_RAW, 0, 0, 1000, 29000 },*/
+        { 268800, CCTL(CLK_TCXO, 1),            SRC_SCPLL, 0x07, 0, 1000, 58000 },
+        { 307200, CCTL(CLK_TCXO, 1),            SRC_SCPLL, 0x08, 0, 1000, 58000 },
+        { 345600, CCTL(CLK_TCXO, 1),            SRC_SCPLL, 0x09, 0, 1025, 58000 },
         { 384000, CCTL(CLK_TCXO, 1),            SRC_SCPLL, 0x0A, 0, 1025, 58000 },
         { 422400, CCTL(CLK_TCXO, 1),            SRC_SCPLL, 0x0B, 0, 1050, 117000 },
         { 460800, CCTL(CLK_TCXO, 1),            SRC_SCPLL, 0x0C, 0, 1050, 117000 },
